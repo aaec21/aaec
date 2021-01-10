672288ca769e64849fa4efa4b7c1a03ca42bf286
Elmehdi <charidielmehdi@gmail.com>
2021-01-10 23:18:22 +0100

    tOP

--- src/middleware/authAdm.js
+++ src/middleware/authAdm.js
@@ -2,7 +2,7 @@ const authAdm = async (req, res, next) => {
     try {
         const ad1 = req.body.ad1;
         const ad2 = req.body.ad2;
-        console.log(process.env.AD1)
+
         if (ad1 === process.env.AD1 && ad2 === process.env.AD2)
             next()
         else throw new Error()
